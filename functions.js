d72a91790572b0a800c202e05ddddd9ff7e6b14f
giovaniv <giovaniv@gmail.com>
2018-06-13 18:42:31 +0000

    added comments in functions.js

--- functions.js
+++ functions.js
@@ -1,8 +1,10 @@
 module.exports = {
 
+  // Function to generate a random alphanumeric string with X
+  // length where X is the parameter
   generateRandomString: function(m) {
-    s = '';
-    r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let s = '';
+    const r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (var i=0; i < m; i++) {
       s += r.charAt(Math.floor(Math.random() * r.length));
     }
