afe1cbe8a8049f1c932bc91c6fa45a0af2ea963f
giovaniv <giovaniv@gmail.com>
2018-06-13 01:39:50 +0000

    added list of urls, views and partials

--- express_server.js
+++ express_server.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var app = express();
-var port = 8080;
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  var num = Math.random() * 100;
+  return num.toString(36).slice(2,8);
+}
+
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.redirect("/urls");
 });
 
 app.get('/urls.json', (req,res) => {
@@ -20,6 +28,10 @@ app.get('/urls.json', (req,res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -32,6 +44,39 @@ app.get('/hello', (req, res) => {
   res.end(html);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+// REDIRECT TO THE ORIGINAL URL (THAT WAS NOT SHORTED)
+app.post('/urls', (req, res) => {
+  var shortLink = generateRandomString();
+  urlDatabase[shortLink] = req.body.longURL;
+  var shortUrl = '/u/'+shortLink;
+  res.redirect(shortUrl);
+});
+
+// DELETE
+app.post('/urls/:id/delete', (req, res) => {
+  var id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
+
+// UPDATE
+app.post('/urls/:id', (req, res) => {
+  var id = req.params.id;
+  urlDatabase[shortLink] = req.body.longURL;
+  res.redirect("/urls");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+
+
+
+
+
